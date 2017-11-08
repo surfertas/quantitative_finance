7e0a3e77eacf5967238dc336962ef69eea7a3649
surfertas <tasuku@gmail.com>
2017-11-08 13:32:00 +0900

    testupdate

--- src/tests/test_pricer.py
+++ src/tests/test_pricer.py
@@ -1,7 +1,8 @@
 from pricer.black_scholes_pricer import *
+from pricer.monte_carlo_pricer import *
 
 
-def test_option_price():
+def test_bs_option_price():
     # Sanity check against http://www.option-price.com/
     s = np.array([100])
     x = np.array([108])
@@ -13,7 +14,7 @@ def test_option_price():
     assert(np.around(p, decimals=5)[0] == 11.21650)
 
 
-def test_greeks():
+def test_bs_greeks():
     s = np.array([100])
     x = np.array([108])
     t, T, r, d, v = (252, 365, 0.05, 0.05, 0.20)
@@ -24,3 +25,11 @@ def test_greeks():
     assert(np.around(Pricer.theta, decimals=5) == -0.01815)
     assert(np.around(Pricer.vega, decimals=5) == 0.29793)
     assert(np.around(Pricer.rho, decimals=5) == 0.21067)
+
+
+def test_mc_pricer():
+    # Test that pricer is producing correct # trials.
+    n_trials = 1000
+    MC = MCPricer(100, 0.05, "normal", T=365)
+    MC.simulate(n_trials, 252, 0.20)
+    assert(len(MC.episodes) == n_trials)
